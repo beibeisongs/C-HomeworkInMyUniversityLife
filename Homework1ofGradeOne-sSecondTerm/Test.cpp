b4ac7b8e794d7633679d881890df8520ab72a1eb
MUJZY <zhengning009@sina.com>
2018-10-02 10:27:36 +0000

    Create Test.cpp

---
+++ Homework1ofGradeOne-sSecondTerm/Test.cpp
@@ -0,0 +1,22 @@
+// Test.cpp
+
+# include<iostream>
+# include"LinkNode.h"
+# include"LinkedList.h"
+# include<string>
+# include"Algorithm.h"
+
+using namespace std;
+
+int main() {
+
+	Algorithm a1;
+
+	a1.factorial(300);
+
+	cout<< "Please input the mark value : "<< endl;
+	int mark;
+	cin>> mark;
+
+	return 0;
+}
