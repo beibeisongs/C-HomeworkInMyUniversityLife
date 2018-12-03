9ed33fe2369d4a704a879591b056d89bccc4de62
MUJZY <zhengning009@sina.com>
2018-12-03 10:29:32 +0800

    Update Test.cpp

--- Homework3InGradeOne-sSecondTerm/Test.cpp
+++ Homework3InGradeOne-sSecondTerm/Test.cpp
@@ -3,10 +3,23 @@
 # include<iostream>
 # include"BinaryTree.h"
 # include"LinkedStack.h"
+# include<vector>
 
 using namespace std;
 
 int main() {
+
+	// 功能测试：前序中序确定二叉树
+	BinaryTree<int> * t1 = new BinaryTree<int>;
+	BinTreeNode<int> * t2 = new BinTreeNode<int>;
+	
+	int arr[7] = {1, 2, 3, 4};
+	vector<int> pre(arr, arr + 4); //从数组中获得初值
+
+	int arr2[7] = {2, 1, 4, 3};
+	vector<int> in(arr2, arr2 + 4); //从数组中获得初值
+
+	t2 = t1->reBuildBinaryTree(pre, in);
 
 	BinaryTree<int> * tree = new BinaryTree<int>;
 	cout<< "Now start creating a binary tree !"<< endl;
